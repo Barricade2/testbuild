fa617d66b01746833a1d94120138d37cf4352c7e
Barricade2 <barri3139@gmail.com>
2023-11-30 20:19:30 +0300

    init commit

--- src/config/settings/environments/production.py
+++ src/config/settings/environments/production.py
@@ -5,12 +5,6 @@ DEBUG = False
 print("prod")
 ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(' ')
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
